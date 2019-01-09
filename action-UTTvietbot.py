aad30dea4df4019d5e68a54bcc6bedb1e1dce1d9
Titanesis <cigolottiaudrey@gmail.com>
2019-01-09 12:14:06 +0100

    correct unicode fail

--- action-UTTvietbot.py
+++ action-UTTvietbot.py
@@ -22,30 +22,30 @@ class VietBot(object):
         self.start_blocking()
 
     def _create_today_menu(self, dish):
-        menu_of_the_day = "Aujourd'hui, tu peux manger {0}.".format(dish)
+        menu_of_the_day = u"Aujourd'hui, tu peux manger {0}.".format(dish)
         return menu_of_the_day
 
     def _create_day_menu(self, day, dish):
-        menu_of_the_day = "{0}, tu pourras manger {1}.".format(day, dish)
+        menu_of_the_day = u"{0}, tu pourras manger {1}.".format(day, dish)
         return menu_of_the_day
 
     def askmenu_callback(self, hermes, intent_message):
         hermes.publish_end_session(intent_message.session_id, "")
 
         en_fr_dict = {
-            "Monday": "Lundi",
-            "Tuesday": "Mardi",
-            "Wednesday": "Mercredi",
-            "Thursday": "Jeudi",
-            "Friday": "Vendredi"
+            u"Monday": u"Lundi",
+            u"Tuesday": u"Mardi",
+            u"Wednesday": u"Mercredi",
+            u"Thursday": u"Jeudi",
+            u"Friday": u"Vendredi"
         }
 
         menu_dict = {
-            "Monday": "des boulettes de boeuf ou du poulet aux légumes",
-            "Tuesday": "du poulet rôti ou du porc sauce piquante",
-            "Wednesday": "du boeuf aux légumes ou des crevettes aigre-douce",
-            "Thursday": "du poulet au curry ou du canard à l'ananas",
-            "Friday": "des boulettes de porc ou du poisson aigre-doux"
+            u"Monday": u"des boulettes de boeuf ou du poulet aux légumes",
+            u"Tuesday": u"du poulet rôti ou du porc sauce piquante",
+            u"Wednesday": u"du boeuf aux légumes ou des crevettes aigre-douce",
+            u"Thursday": u"du poulet au curry ou du canard à l'ananas",
+            u"Friday": u"des boulettes de porc ou du poisson aigre-doux"
         }
 
         if intent_message.slots.menuday:
@@ -62,7 +62,7 @@ class VietBot(object):
         else:
             sentence = self._create_today_menu(menu_dict[day_name])
 
-        hermes.publish_start_session_notification(intent_message.site_id, sentence, "VietBot_FR")
+        hermes.publish_start_session_notification(intent_message.site_id, sentence, u"VietBot_FR")
 
     # --> Master callback function, triggered everytime an intent is recognized
     def master_intent_callback(self, hermes, intent_message):
